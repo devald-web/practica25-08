edce427e3b7eb44400194321d5828996bc7dd658
devald-web <1437558@senati.pe>
2025-08-25 19:46:25 -0500

    MOD: Se agregó formulario y extras en css e interactividad js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hola desde LATAM");
+
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const lastname = document.getElementById("lastname").value;
+    alert(`Nombre: ${name} ${lastname}`);
+});
